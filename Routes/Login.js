b5727e7c82f4cd129666af37a5f0cac89ef123ca
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-01-03 23:30:32 +0500

    login api constructed

--- Routes/Login.js
+++ Routes/Login.js
@@ -12,9 +12,10 @@ Router.post(
         body('Password').exists()
     ],
     async (req, res)=>{
-const validationResult=validationResult(req)
+const validationResult=validationResult(req);
+let success=false;
 if(validationResult.isEmpty()){
-    
+    res.json({success, msg:'please enter currrent currendtionl'})
 }
     }
 )
