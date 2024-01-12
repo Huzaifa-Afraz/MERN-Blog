deb0e96d879dc80a05c1c2af3cda325277563f33
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-01-12 23:35:27 +0500

    show user specific blog api coplete

--- Routes/show-blog.js
+++ Routes/show-blog.js
@@ -1,15 +1,15 @@
 const express=require('express');
 const router=express.Router();
-const logedInUser=require('../middleware/fetchuser');
+const loggedInUser=require('../middleware/fetchuser');
 const Blog=require('../Models/Blog')
-router.get('/show-blogs',logedInUser, async(req,res)=>{
+router.get('/show-blogs',loggedInUser, async(req,res)=>{
 let success=false;
 const user=req.user;
 console.log(user)
     if(!user){
         return res.status(400).json({success, msg:'user not found'})
     }
-    const blogs= await Blog.findById(user);
+    const blogs= await Blog.find({userid:user});
     return blogs!==null?res.send({blogs,user}):res.send({user,msg:'blogs not found for this id'});
 })
 
